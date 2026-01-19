9c7e8d87a6c062268f21d32427a3797aef22a123
13777470884@qq.com <13777470884@qq.com>
2026-01-19 22:44:27 +0800

    简单的配置信息

--- problem3795/src/test/java/cn/edu/hdu/acm/problem3795/Problem3795ApplicationTests.java
+++ problem3795/src/test/java/cn/edu/hdu/acm/problem3795/Problem3795ApplicationTests.java
@@ -1,13 +1,21 @@
 package cn.edu.hdu.acm.problem3795;
 
+import cn.edu.hdu.acm.problem3795.service.PerComputeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Problem3795ApplicationTests {
 
+    @Autowired
+    private PerComputeService service;
+
     @Test
     void contextLoads() {
+        service.generateAll();
     }
+
+
 
 }
