8ddf5ccab40e3c8722b486ef475c690d64cfd456
13777470884@qq.com <13777470884@qq.com>
2026-01-28 19:58:58 +0800

    [ADD] problem1629

---
+++ problem1629/src/main/java/cn/edu/hdu/acm/problem1629/Problem1629Application.java
@@ -0,0 +1,13 @@
+package cn.edu.hdu.acm.problem1629;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problem1629Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Problem1629Application.class, args);
+    }
+
+}
---
+++ problem1629/src/test/java/cn/edu/hdu/acm/problem1629/Problem1629ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hdu.acm.problem1629;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Problem1629ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
