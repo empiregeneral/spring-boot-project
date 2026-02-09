c57fd80981d261848d163f2ec1dc9bb4b79af4a0
empiregeneral <13777470884@qq.com>
2026-02-10 03:46:15 +0800

    [BugFix] Service不能返回controller的结果

--- problem1111/src/main/java/cn/pintia/zjo/problem1111/service/PokerService.java
+++ problem1111/src/main/java/cn/pintia/zjo/problem1111/service/PokerService.java
@@ -1,6 +1,5 @@
 package cn.pintia.zjo.problem1111.service;
 
-import cn.pintia.zjo.problem1111.dto.response.PokerCompareResponse;
 import cn.pintia.zjo.problem1111.entity.CardUtils;
 import cn.pintia.zjo.problem1111.enumeration.Judgement;
 import cn.pintia.zjo.problem1111.factory.PokerHandFactory;
@@ -12,24 +11,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PokerService  {
-    private String player1Hands;
-    private String player2Hands;
-    private PokerHandType player1HandsType;
-    private PokerHandType player2HandsType;
-
     @Autowired
     private PokerHand pokerHand;
 
     public Triplet<Judgement, String, String> compareHands(String player1Hands, String player2Hands) {
-        this.player1Hands = player1Hands;
-        this.player2Hands = player2Hands;
-        this.player1HandsType = pokerHand.pokerHandType(this.player1Hands);
-        this.player2HandsType = pokerHand.pokerHandType(this.player2Hands);
+
+        PokerHandType player1HandsType = pokerHand.pokerHandType(player1Hands);
+        PokerHandType player2HandsType = pokerHand.pokerHandType(player2Hands);
 
         int cmp = player1HandsType.compareTo(player2HandsType);
         PokerHandType pokerHandType = player1HandsType.getHandType();
-        Judgement judgement = PokerHandFactory.showHandJudgement(CardUtils.toCards(this.player1Hands),
-                CardUtils.toCards(this.player2Hands), pokerHandType);
+        Judgement judgement = PokerHandFactory.showHandJudgement(CardUtils.toCards(player1Hands),
+                CardUtils.toCards(player2Hands), pokerHandType);
         if (cmp == 0) {
             return new Triplet<>(judgement, " Same PokerHandType, base on poker faces or kickers", judgement.getDescription());
         } else if (cmp < 0) {
