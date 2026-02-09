6f8178f32e49af2ea1f9f6bdf3f6b71087fcd13b
empiregeneral <13777470884@qq.com>
2026-02-10 03:43:56 +0800

    [BugFix] Service不能返回controller的结果

--- problem1111/src/main/java/cn/pintia/zjo/problem1111/dto/response/PokerCompareResponse.java
+++ problem1111/src/main/java/cn/pintia/zjo/problem1111/dto/response/PokerCompareResponse.java
@@ -3,9 +3,9 @@ package cn.pintia.zjo.problem1111.dto.response;
 import cn.pintia.zjo.problem1111.enumeration.Judgement;
 
 public class PokerCompareResponse {
-    private Judgement result;
-    private String reason;
-    private String winner; // "player1", "player2", "tie"
+    private final Judgement result;
+    private final String reason;
+    private final String winner; // "player1", "player2", "tie"
 
     public PokerCompareResponse(Judgement result, String reason, String winner) {
         this.result = result;
