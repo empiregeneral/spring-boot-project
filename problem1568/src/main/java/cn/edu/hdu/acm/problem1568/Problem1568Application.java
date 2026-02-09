3853af72f3cc4760710932ad977b6e4392004234
empiregeneral <13777470884@qq.com>
2026-02-10 04:47:23 +0800

    [ADD] 核心service

---
+++ problem1568/src/main/java/cn/edu/hdu/acm/problem1568/Problem1568Application.java
@@ -0,0 +1,13 @@
+package cn.edu.hdu.acm.problem1568;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problem1568Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Problem1568Application.class, args);
+    }
+
+}
